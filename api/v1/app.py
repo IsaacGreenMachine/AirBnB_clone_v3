386cf0d56e9501466b4256129e3011ae2ce8569e
Isaac <zuderg@gmail.com>
2021-12-16 14:45:45 -0600

    added task 4 with correct json formatting and shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,18 @@
-"""app module for RESTful interface"""
-from os import getenv 
+#!/usr/bin/python3
+from os import getenv
 from flask import Flask
-app = Flask(__name__)
 from models import storage
 from views import app_views
+"""app module for RESTful interface"""
+app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == "__main__":
     hostVal = getenv("HBNB_API_HOST")
@@ -17,6 +20,5 @@ if __name__ == "__main__":
         hostVal = '0.0.0.0'
     portVal = getenv("HBNB_API_PORT")
     if getenv("HBNB_API_PORT") is None:
-        portVal = '5000'    
+        portVal = '5000'
     app.run(host=hostVal, port=portVal, threaded=True)
-
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
 from api.v1.views import app_views
+import json
+
 
 @app_views.route('/status')
 def okayThen():
-    return '"status": "OK"'
+    return json.dumps({"status": "OK"})
