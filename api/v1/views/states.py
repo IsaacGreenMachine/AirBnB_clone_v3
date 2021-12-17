3eba3f62ff404ce945cb1e41068a82cdc342887d
Tres Serio <treserio@hotmail.com>
2021-12-17 16:45:44 -0600

    added half of task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,23 +1,27 @@
-#must use obj.todict?
+# must use obj.todict?
 from sqlalchemy.sql.functions import mode
 from api.v1.views import app_views, modelsDict
 from models import storage
 import flask
 
 
-
 @app_views.route('/states', methods=['GET'])
 @app_views.route('/states/<id>', methods=['GET'])
-def routeFn(cls, id=None):
+def st_get(id=None):
     if id is None:
-        return flask.jsonify(storage.all(modelsDict['states']))
+        return flask.jsonify([ob.to_dict() for ob in storage.all(modelsDict['states']).values()])
 
     try:
-        if cls == 'states':
-            cls = "State"
-            st = storage.all()[cls + '.' + id]
-            return flask.jsonify(st.__dict__)
-
+        return flask.jsonify(storage.all()['State.' + id].to_dict())
     except Exception:
         flask.abort(404)
 
+
+@app_views.route('/states/<id>', methods=['DELETE'])
+def st_del(id=None):
+    if storage.get("State", id):
+        storage.get('State', id).delete()
+        storage.save()
+        return flask.make_response({}, 200)
+    else:
+        flask.abort(404)
