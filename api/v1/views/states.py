bf4e6220e9637ef6faa2a81459fcec1a01f24d12
Tres Serio <treserio@hotmail.com>
2021-12-18 12:58:03 -0600

    +POST

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,4 @@
 # must use obj.todict?
-from sqlalchemy.sql.functions import mode
 from api.v1.views import app_views, modelsDict
 from models import storage
 import flask
@@ -25,3 +24,16 @@ def st_del(id=None):
         return flask.make_response({}, 200)
     else:
         flask.abort(404)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def st_post():
+    new_st = flask.request.get_json()
+    if not new_st:
+        flask.abort(404, 'Not a JSON')
+    if 'name' not in new_st.keys():
+        flask.abort(404, 'Missing name')
+    st = modelsDict['states'](**new_st)
+    storage.new(st)
+    storage.save()
+    return flask.make_response(st.to_dict(), 200)
