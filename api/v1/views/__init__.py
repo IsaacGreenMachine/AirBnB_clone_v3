ddedf7bb0eedfd751ecfe9d32cd354ac12b2bfc4
Tres Serio <treserio@hotmail.com>
2021-12-20 21:31:54 -0600

    +amenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -22,3 +22,4 @@ app_views = Blueprint("/api/v1", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *  # keep below app_views to resolve circular
 import api.v1.views.states
 import api.v1.views.cities
+import api.v1.views.amenities
